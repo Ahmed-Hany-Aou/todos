eb7bb9367baf00a4acfe2e152bc754352da6df97
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-08-02 17:05:10 +0300

    managed to create new todo and view it----please notice there is a huge difference using laravel verison 6 and laravel verison 8--check web.php comments

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodosController;
-
 Route::get('/', function () {
-    return view('todos.welcome');
+    return view('welcome');
 });
 
-Route::resource('todos', TodosController::class);
+// Laravel 8+ recommended way to reference controllers
+use App\Http\Controllers\TodosController; /// we need to use this code to reference controllers, it is not mentioned in the course ----------
+
+Route::get('todos', [TodosController::class, 'index']);
+Route::get('todos/{todo}', [TodosController::class, 'show']);
+Route::get('new-todos', [TodosController::class, 'create']);
+Route::post('store-todos', [TodosController::class, 'store']);
