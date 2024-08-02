bac2fbcb9e41079cd226326b5ec64d1d57bc731d
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-08-03 00:53:04 +0300

    added route model biding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -12,9 +12,9 @@ class TodosController extends Controller
       return view('todos.index')->with('todos', Todo::all());
     }
 
-    public function show($todoId)
+    public function show(Todo $todo)
     {
-      return view('todos.show')->with('todo', Todo::find($todoId));
+      return view('todos.show')->with('todo', $todo);
     }
 
     public function create()
@@ -46,22 +46,22 @@ class TodosController extends Controller
       $todo=Todo:: find($todoId);
       return view('todos.edit')->with('todo', $todo);
     }
-    public function update($todoId)
+    public function update(Todo $todo)
     {
       $this->validate(request(), [
         'name' => 'required|min:6|max:12',
         'description' => 'required'
       ]);
       $data = request()->all();
-      $todo = Todo::find($todoId);
+      
       $todo->name = $data['name'];
       $todo->description = $data['description'];
       $todo->save();
       return redirect('/todos');
 }
-public function destroy($todoId)
+public function destroy(Todo $todo)
 {
-$todo = Todo::find($todoId);
+
 $todo->delete();
  // Todo::destroy($todoId);
  return redirect('/todos');
